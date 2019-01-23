e368c1668cc7ff1f8896724ce9430836ad81636f
sefabckn <31974256+sefabckn@users.noreply.github.com>
2019-01-23 14:55:06 +0300

    Create core_algrothim.py

---
+++ core_algrothim.py
@@ -0,0 +1,14 @@
+############################################
+#  *Core Algortihms                        #
+#  *Author:Benj4min                        #
+#                                          #
+#                                          #
+#                                          #
+############################################
+
+
+#finding max value in a list algorithm
+def max_val(list):
+
+
+
