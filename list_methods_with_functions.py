29d783b490a8495ead91d5e90ba4836726d9426d
sefabckn <31974256+sefabckn@users.noreply.github.com>
2019-01-23 15:26:51 +0300

    Create list_methods_with_functions.py

---
+++ list_methods_with_functions.py
@@ -0,0 +1,27 @@
+###########################################################
+#  *In This tutorial i will write some list methods       #
+#   using functions for to improve my function skills     #
+#                                                         #
+#   *Author:Benj4min                                      #
+#                                                         #
+###########################################################
+
+
+"""
+Firstly i would like to explain this method stuffs.
+In Python lists we have some methods (append,pop,remove...)
+these methods is providing to make some process more simple.
+for example:
+list1 = [1,2,3,4,5,6,"Python"]
+if you want to add something to in this list.There is a simple method in python as
+list1.append("Learning Programming")
+now if we check our list1;
+[1,2,3,4,5,6,"Python","Learning Programming"]
+"""
+
+"""
+Now I will coding some simple methods,LEts start
+"""
+
+def clear():
+
